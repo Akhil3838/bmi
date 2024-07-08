a65ba29b9233429ad464ce15285e4baee94a58fc
akhil3838 <akhiltv713@gmail.com>
2024-07-08 11:28:33 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,255 @@
+import './App.css'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+import GaugeComponent from 'react-gauge-component'
+import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
+import Avatar from '@mui/material/Avatar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+
+
+
+function App() {
+
+  // width
+  const Input = styled(MuiInput)`
+  width: 142px;
+`;
+
+  const [value, setValue] = React.useState(0);
+
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? 0 : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+
+
+  //height 
+  const [height, setHeight] = React.useState(0);
+
+  const handleHeightSliderChange = (event, newValue) => {
+    setHeight(newValue);
+  };
+
+  const handleHeightInputChange = (event) => {
+    setHeight(event.target.value === '' ? 0 : Number(event.target.value));
+  };
+
+  const handleHeightBlur = () => {
+    if (height < 0) {
+      setHeight(0);
+    } else if (height > 200) {
+      setHeight(200);
+    }
+  };
+  // reset and calculate
+
+  const [BMI,setBMI] = React. useState(0)
+
+  const handleRest=()=>{
+    setHeight('0')
+    setValue('0')
+    setBMI('0')
+    
+  }
+  
+  const result=()=>{
+     setBMI((value / (height / 100) ** 2).toFixed(1))
+     instruction(BMI);
+
+  }
+  //condition
+  const [category,setcategory]=React.useState("condition")
+  const [discription,setdiscription]= React.useState("solution for it")
+
+  const instruction=(BMI)=>{
+    let category ='';
+    let discription = '';
+   
+    if (BMI<=18.5) {
+
+      category='UNDERWEIGHT';
+      discription='Eat healthy foods';
+      
+    }
+    else if(BMI>=18.5 && BMI<=24.9){
+      category='HEALTHY !';
+      discription='Stay healthy';
+
+    }
+    else if(BMI>=25.0 && BMI<=39.9){
+      category='OVERWEIGHT';
+      discription='reduced-calorie diet and exercise regularly';
+
+
+    }
+  
+   
+ else {
+     category="OBESE !";
+      discription='go to medical checkup';
+    
+  }
+  setcategory(category)
+  setdiscription(discription);
+};
+
+
+  
+  
+
+
+  return (
+    <>
+<div className='d-flex align-items-center justify-content-center'>
+        <div className="row w-75">
+          <div className="col-md-2"></div>
+          <div className="col-md-8 ">
+
+              <div className="row  p-3 mb-5 shadow-lg  mt-5  rounded-4 p-3 py-4 " style={{backgroundColor:'#1A2421'}}>
+              
+             <h2 className='text-center mt-3 fw-bold text-white '>BMI calculator</h2>
+              <div className="col-md-6 mt-3">
+             <div>
+                  <GaugeComponent className='w-100  '
+                    value={BMI}
+                    type="radial"
+                    labels={{
+                      valueLabel: {
+                        formatTextValue: (value) => `${BMI}`,
+                      },
+                      tickLabels: {
+                        type: "inner",
+                        ticks: [
+                          { value: 20 },
+                          { value: 40 },
+                          { value: 60 },
+                          { value: 80 },
+                          { value: 100 }
+                        ]
+                      }
+                    }}
+                    arc={{
+                      colorArray: ['#5BE12C', '#EA4228'],
+                      subArcs: [{ limit: 10 }, { limit: 30 }, {}, {}, {}],
+                      padding: 0.02,
+                      width: 0.3
+                    }}
+                    pointer={{
+                      elastic: true,
+                      animationDelay: 0
+                    }}
+                  />
+                 <div className='w-100 text-center'>
+                    <h4 className=' fw-bold text-success '>{category}</h4>
+                    <p className='text-white'>{discription}</p>
+                 </div>
+    
+               </div>           
+                </div>
+              <div className="col-md-6 mt-3">
+              <div className='mt-2'>
+                <Chip  style={{paddingRight:'18px', color:'black', backgroundColor:'whitesmoke'}} avatar={<Avatar   src="https://img.freepik.com/premium-vector/boy-with-dental-braces-dental-care-little-boy-portrait-circular-frame_254685-975.jpg"  />} 
+                 label="Male" />
+                <Chip  style={{paddingRight:'14px' ,color:'black', backgroundColor:'whitesmoke'}} className='ms-md-3' avatar={<Avatar   src="https://img.freepik.com/premium-vector/little-smiling-girl-cute-portrait-child-vector-illustration_254685-2049.jpg"  /> }
+                 label="Female"  />
+    
+             </div> 
+               {/* width */}
+                <div style={{ width: "250px" }} className='mt-4' >
+                  <Typography id="input-slider" gutterBottom>
+                   <span className='fw-bolder text-white'> weight</span>
+                   <FontAwesomeIcon icon={faArrowRight} fade className='ms-2 text-white' />
+                  </Typography>
+                 <div className=''>
+                    <Slider 
+                      value={typeof value === 'number' ? value : 0}
+                      onChange={handleSliderChange}
+                      aria-labelledby="input-slider"
+                    />
+                 </div>
+                  <Input className='text-white shadow-lg'
+                    value={value}
+                    size="small"
+                    variant="standard"
+                    color=''
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                    inputProps={{
+                      step: 10,
+                      min: 0,
+                      max: 100,
+                      type: 'number',
+                      'aria-labelledby': 'input-slider',
+                    }}
+                  />
+                </div>
+  
+                {/* height */}
+  
+                <div style={{ width: '250px', marginTop: '20px' }}>
+                  <Typography id="height-slider" gutterBottom>
+                   <span className='fw-bolder text-white'> Height</span>
+                    <FontAwesomeIcon icon={faArrowRight} fade className='ms-2 text-white' />
+                  </Typography>
+                  <Slider 
+                    value={typeof height === 'number' ? height : 0}
+                    onChange={handleHeightSliderChange}
+                    aria-labelledby="height-slider"
+                    min={0}
+                    max={200}
+                    step={1}
+                  />
+                  <Input className='text-white shadow-lg'
+                    value={height}
+                    size="small"
+                    onChange={handleHeightInputChange}
+
+                    onBlur={handleHeightBlur}
+                    inputProps={{
+                      step: 2,
+                      min: 0,
+                      max: 200,
+                      type: 'number',
+                      'aria-labelledby': 'height-slider',
+                    }}
+                  />
+                </div>
+                <div className='mt-3'>
+                <Button className=' fw-bold  border-white text-white border-1' variant="outlined" type='submit' size='mediam' onClick={result}>CALCULATE</Button>
+                <Button  variant="outlined" className='  ms-3 text-black border-white text-white'  size='mediam' onClick={handleRest}>RESET</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2"></div>
+        </div>
+  
+  
+</div>
+
+
+
+    </>
+  )
+}
+
+export default App
