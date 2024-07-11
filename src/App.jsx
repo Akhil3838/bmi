dbb72f7b12bfa8426b37899b888f8e4290740952
akhil3838 <akhiltv713@gmail.com>
2024-07-11 19:08:26 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,10 @@ function App() {
   // reset and calculate
 
   const [BMI,setBMI] = React. useState(0)
+  const [category,setcategory]=React.useState("condition")
+  const [discription,setdiscription]= React.useState("instruction to be")
+  const [src,setsrc]=useState("/obs.gif")
+
 
   const handleRest=()=>{
     setHeight('0')
@@ -71,51 +75,47 @@ function App() {
     
   }
   
+  
   const result=()=>{
-     setBMI((value / (height / 100) ** 2).toFixed(1))
-     instruction(BMI);
+     let rel=((value / (height / 100) ** 2).toFixed(1))
+     setBMI(rel)
 
-  }
-  //condition
-  const [category,setcategory]=React.useState("condition")
-  const [discription,setdiscription]= React.useState("solution for it")
+      if (rel<=18.5) {
+  
+        setcategory('UNDERWEIGHT');
+        setdiscription('Eat healthy foods');
+        setsrc("/food-yummy.gif")
+        
+      }
+      else if(rel>=18.5 && rel<24.9){
+        setcategory('HEALTHY !');
+        setdiscription('Stay healthy');
+        setsrc("/normalweight.gif")
 
-  const instruction=(BMI)=>{
-    let category ='';
-    let discription = '';
-   
-    if (BMI<=18.5) {
+  
+      }
+      else if(rel>=25.0 && rel<39.9){
+        setcategory('OVERWEIGHT');
+        setdiscription('reduced-calorie diet and exercise regularly');
+        setsrc("/workout.gif")
 
-      category='UNDERWEIGHT';
-      discription='Eat healthy foods';
+      }
+    
+     
+   else {
+       setcategory("OBESE !");
+        setdiscription('go to medical checkup');
+        setsrc("/obs.gif")
+
       
     }
-    else if(BMI>=18.5 && BMI<=24.9){
-      category='HEALTHY !';
-      discription='Stay healthy';
-
-    }
-    else if(BMI>=25.0 && BMI<=39.9){
-      category='OVERWEIGHT';
-      discription='reduced-calorie diet and exercise regularly';
-
-
-    }
   
-   
- else {
-     category="OBESE !";
-      discription='go to medical checkup';
-    
+  
+  
+
   }
-  setcategory(category)
-  setdiscription(discription);
-};
-
 
   
-  
-
 
   return (
     <>
@@ -124,12 +124,14 @@ function App() {
           <div className="col-md-2"></div>
           <div className="col-md-8 ">
 
-              <div className="row  p-3 mb-5 shadow-lg  mt-5  rounded-4 p-3 py-4 " style={{backgroundColor:'#1A2421'}}>
+              <div className="row  p-3 mb-5 shadow-lg  mt-5  rounded-4 p-3 py-4 " data-aos="fade-right"
+                data-aos-duration="1000" style={{backgroundColor:'#1A2421', opacity: '0.9'}}>
               
-             <h2 className='text-center mt-3 fw-bold text-white '>BMI calculator</h2>
-              <div className="col-md-6 mt-3">
+             <h2 className='text-center mt-3 fw-bold text-white '  data-aos="fade-left"
+              data-aos-duration="1000">BMI calculator</h2>
+              <div className="col-md-6 "> 
              <div>
-                  <GaugeComponent className='w-100  '
+                  <GaugeComponent  className='w-100  '
                     value={BMI}
                     type="radial"
                     labels={{
@@ -159,14 +161,17 @@ function App() {
                     }}
                   />
                  <div className='w-100 text-center'>
-                    <h4 className=' fw-bold text-success '>{category}</h4>
-                    <p className='text-white'>{discription}</p>
+                    <h4 className=' fw-bold text-success ' data-aos="fade-left"
+              data-aos-duration="1000">{category}</h4>
+                       <img src={src} alt="" width={'60px'} height={'50px'}/>
+                    <p className='text-white' data-aos="fade-left"
+              data-aos-duration="1000">{discription}</p>
                  </div>
     
                </div>           
                 </div>
-              <div className="col-md-6 mt-3">
-              <div className='mt-2'>
+              <div className="col-md-6 mt-3 ">
+              <div className='mt-2 '>
                 <Chip  style={{paddingRight:'18px', color:'black', backgroundColor:'whitesmoke'}} avatar={<Avatar   src="https://img.freepik.com/premium-vector/boy-with-dental-braces-dental-care-little-boy-portrait-circular-frame_254685-975.jpg"  />} 
                  label="Male" />
                 <Chip  style={{paddingRight:'14px' ,color:'black', backgroundColor:'whitesmoke'}} className='ms-md-3' avatar={<Avatar   src="https://img.freepik.com/premium-vector/little-smiling-girl-cute-portrait-child-vector-illustration_254685-2049.jpg"  /> }
@@ -174,19 +179,20 @@ function App() {
     
              </div> 
                {/* width */}
-                <div style={{ width: "250px" }} className='mt-4' >
+                <div  style={{ width: "250px" }} className='mt-4' >
                   <Typography id="input-slider" gutterBottom>
-                   <span className='fw-bolder text-white'> weight</span>
+                   <span className='fw-bolder text-white ms-2'> weight</span>
                    <FontAwesomeIcon icon={faArrowRight} fade className='ms-2 text-white' />
                   </Typography>
-                 <div className=''>
+                 <div className='px-2'data-aos="fade-left"
+              data-aos-duration="1000">
                     <Slider 
                       value={typeof value === 'number' ? value : 0}
                       onChange={handleSliderChange}
                       aria-labelledby="input-slider"
                     />
                  </div>
-                  <Input className='text-white shadow-lg'
+                  <Input className='text-white shadow-lg ms-2'
                     value={value}
                     size="small"
                     variant="standard"
@@ -205,20 +211,23 @@ function App() {
   
                 {/* height */}
   
-                <div style={{ width: '250px', marginTop: '20px' }}>
+                <div  style={{ width: '250px', marginTop: '20px' }}>
                   <Typography id="height-slider" gutterBottom>
-                   <span className='fw-bolder text-white'> Height</span>
+                   <span className='fw-bolder text-white ms-2'> Height</span>
                     <FontAwesomeIcon icon={faArrowRight} fade className='ms-2 text-white' />
                   </Typography>
-                  <Slider 
-                    value={typeof height === 'number' ? height : 0}
-                    onChange={handleHeightSliderChange}
-                    aria-labelledby="height-slider"
-                    min={0}
-                    max={200}
-                    step={1}
-                  />
-                  <Input className='text-white shadow-lg'
+               <div className='px-2'data-aos="fade-left"
+              data-aos-duration="1000">
+                    <Slider 
+                      value={typeof height === 'number' ? height : 0}
+                      onChange={handleHeightSliderChange}
+                      aria-labelledby="height-slider"
+                      min={0}
+                      max={200}
+                      step={1}
+                    />
+               </div>
+                  <Input className='text-white shadow-lg ms-2'
                     value={height}
                     size="small"
                     onChange={handleHeightInputChange}
@@ -233,7 +242,7 @@ function App() {
                     }}
                   />
                 </div>
-                <div className='mt-3'>
+                <div className='mt-5'>
                 <Button className=' fw-bold  border-white text-white border-1' variant="outlined" type='submit' size='mediam' onClick={result}>CALCULATE</Button>
                 <Button  variant="outlined" className='  ms-3 text-black border-white text-white'  size='mediam' onClick={handleRest}>RESET</Button>
                 </div>
